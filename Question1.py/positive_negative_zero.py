ee4e567364b0d2eda7a75bd3d7dda6832286030e
shrutikl0007 <145192005+shrutikl0007@users.noreply.github.com>
2023-09-16 14:18:17 +0530

    Add files via upload

---
+++ Question1.py/positive_negative_zero.py
@@ -0,0 +1,11 @@
+# Question:1
+# a python program check if a number is positive,negative and zero:
+
+
+number = float(input("enter the number of x:"))
+if number>0:
+    print("this number is positive.")
+elif number<0:
+    print("this number is negative.")
+else:
+    print("this number is zero.")
